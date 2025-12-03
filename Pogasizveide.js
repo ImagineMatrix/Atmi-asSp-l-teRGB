ee9c58d0496abafe998c7e560e714a9592ec7040
evolberga <evolberga@DXRDA201693.ri.riga.lv>
2025-12-03 16:43:15 +0200

    ar skolotaju laboju

--- Pogasizveide.js
+++ Pogasizveide.js
@@ -2,10 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-        
+       
 function izveido(){
+                   
+                   function shuffle(array) {
+                   array.sort(() => Math.random() - 0.5);
+               }
+
+               function randomPairs(k){
+                   let n = Document.getElementById("forma").value;
+                   let randomi=new Set();
+                   let randomBildes=[];
+                    n=bildes1.length;
+                   while(randomi.size<k){
+                       randomi.add(Math.floor(Math.random()*n));
+                   }
+                   randomi.forEach((item)=>{
+                       randomBildes.push(bildes1[item]);
+                       randomBildes.push(bildes2[item]);
+                   });
+                   return randomBildes;
+               }
 
 
+               let bildes1 = ["a1.png","a2.png","a3.png","a4.png","a5.png","a6.png", "a7.png", "a8.png"];
+               let bildes2 = ["(255, 0, 0)","(0, 255, 0)","(0, 0, 255)","(255, 255, 0)","(255, 255, 255)","(0, 0, 0)", "(255, 0, 255)", "(0, 255, 255)"];
+
+               let bildes;
+               if(n === 2){
+               bildes = randomPairs(2);
+               }
+               else if (n === 4) {
+               bildes = randomPairs(8);
+               }
+
+
+               shuffle(bildes);
+
+               for (let i in bildes) {
+                  console.log(bildes[i]);
+                  }
+            
+            
             let kollona;
          if(document.getElementById("pogaslaukums") !== null){
              document.getElementById("pogaslaukums").innerHTML = "";
@@ -16,7 +54,7 @@ function izveido(){
             //set atribute
         }
           document.getElementById("alerts").innerHTML = "";
-          let n = parseInt(document.getElementById('forma').value);
+          n = parseInt(document.getElementById('forma').value);
           if(n <= 0){
               document.getElementById("alerts").innerHTML = "Ievadi pozitīvu skaitli";
               return 0;
@@ -32,9 +70,18 @@ function izveido(){
                 let kollona = document.getElementById("kollona");   
                 let poga = document.createElement("INPUT");
                 poga.type = "button";
+                
+                if (bildes[i*n + j].startsWith("a")){
                 poga.style.backgroundImage = "url("+urrl + bildes[i*n + j]+")";
-                poga.style.width =  n + "8px";
-                poga.style.height = n + "8px";
+                console.log("hey");
+                 }
+                else {
+                poga.value = bildes[i*n + j];
+                }
+                
+
+                poga.style.width = "40%";
+                poga.style.height = n/2 + "25px";
                 poga.style.margin = "15px";
                 poga.style.border = "none";
                 kollona.style.paddingTop = "50px";
@@ -56,49 +103,20 @@ function izveido(){
           } 
   
 
-let sekundes = 60;
-// funkcija atkārtoti tiks izsaukta ik pēc 1000 milisekundēm
-setInterval(function(){
-   document.getElementById("laiks").innerHTML= sekundes+" s ";
-   if (sekundes>0) sekundes--;   
-},1000);
-
-// funkcijas izsaukšana tiek atlikta uz 11000 milisekundēm 
- setTimeout(function(){
-    document.getElementById("laiks").style.backgroundColor="red";
- },11000);
-
-
-    function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-}
+//let sekundes = 60;
+//// funkcija atkārtoti tiks izsaukta ik pēc 1000 milisekundēm
+//setInterval(function(){
+//   document.getElementById("laiks").innerHTML= sekundes+" s ";
+//   if (sekundes>0) sekundes--;   
+//},1000);
+//
+//// funkcijas izsaukšana tiek atlikta uz 11000 milisekundēm 
+// setTimeout(function(){
+//    document.getElementById("laiks").style.backgroundColor="red";
+// },11000);
 
 
-function randomPairs(k){
-    let randomi=new Set();
-    let randomBildes=[];
-    let n=bildes1.length;
-    while(randomi.size<k){
-        randomi.add(Math.floor(Math.random()*n));
-    }
-    randomi.forEach((item)=>{
-        randomBildes.push(bildes1[item]);
-        randomBildes.push(bildes2[item]);
-    });
-    return randomBildes;
-}
-
-
-let bildes1 = ["a1.png","a2.png","a3.png","a4.png","a5.png","a6.png"];
-let bildes2 = ["(255, 0, 0)","(0, 255, 0)","(0, 0, 255)","(255, 255, 0)","(255, 255, 255)","(0, 0, 0)"];
-
-let bildes=randomPairs(4);
-
-shuffle(bildes);
-
-for (let i in bildes) {
-   console.log(bildes[i]);
-   }
+   
 
 
 
